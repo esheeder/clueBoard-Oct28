558e89c762834845dac1b30a7ec6bae69a12fc40
Dezmon Fernandez <defernan@mines.edu>
2014-09-26 19:00:28 -0600

    created bare bones bad exceptino config class

---
+++ src/clueGame/BadConfigFormatException.java
@@ -0,0 +1,17 @@
+package clueGame;
+
+public class BadConfigFormatException extends Exception{
+
+	public BadConfigFormatException(String message) {
+		super(message);
+	}
+	public BadConfigFormatException(){
+		
+	}
+	@Override
+	public String toString() {
+		return "Bad config file.....";
+	}
+	
+
+}
