29431feec270ebeb997a3d95cb7db696bf0a0ce9
Dezmon Fernandez <atdez@dezmons-mac.hsd1.co.comcast.net>
2014-09-29 18:47:24 -0600

    added walkway, not sure what happened to it

---
+++ src/clueGame/Walkway.java
@@ -0,0 +1,17 @@
+package clueGame;
+
+public class Walkway extends BoardCell{
+	
+	public Walkway() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public boolean isWalkway(){
+		return true;
+	}
+	@Override
+	public void draw(){
+		
+	}
+
+}
