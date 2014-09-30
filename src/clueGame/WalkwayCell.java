55e933634abf4298d24dddde563257002ee93898
Isaac Valdez <ivaldez@mymail.mines.edu>
2014-09-30 11:11:46 -0600

    added walkway

---
+++ src/clueGame/WalkwayCell.java
@@ -0,0 +1,16 @@
+package clueGame;
+
+public class WalkwayCell extends BoardCell{
+
+	public WalkwayCell() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public boolean isWalkway(){
+		return true;
+	}
+	@Override
+	public void draw(){
+		
+	}
+}
