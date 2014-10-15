131a8e92466e750b4bd51c14f43d2fb29229936a
Alex <apatel@mines.edu>
2014-10-15 15:32:53 -0600

    implemented load players

---
+++ src/clueTests/GameSetUpTest.java
@@ -0,0 +1,79 @@
+package clueTests;
+
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import clueGame.Board;
+import clueGame.ComputerPlayer;
+import clueGame.HumanPlayer;
+import clueGame.Player;
+
+public class GameSetUpTest {
+	private static Board board;
+	private static ArrayList<Player> players; 
+	// set up the board and a variety of cards
+	@Before
+	public void setUp() {
+		board = new Board();
+		players = new ArrayList<Player>();
+		FileReader reader= null;
+		Scanner in = null;
+		String line = null;
+		try{
+			reader = new FileReader("clueBoard/Players.csv");
+			in = new Scanner(reader);
+		}catch(FileNotFoundException e){
+			System.out.println(e.getLocalizedMessage());
+		}
+		
+		while(in.hasNextLine()) {
+			line = in.nextLine();
+			String [] entireline = line.split(",");
+			
+			if(entireline[0].equals("H")) {
+				HumanPlayer h = new HumanPlayer(entireline[1], entireline[2], Integer.parseInt(entireline[3]),Integer.parseInt(entireline[4]));
+				players.add(h);
+			}
+			else {
+				ComputerPlayer c = new ComputerPlayer(entireline[1], entireline[2], Integer.parseInt(entireline[3]),Integer.parseInt(entireline[4]));
+				players.add(c);
+			}
+			
+		}
+		
+	}
+
+	
+	// Load the people from: 'Players.csv'
+	// format: H/C, name, color, x, y
+	@Test
+	public void loadPeopleTest() {
+		
+		// test the size of the list of players
+		assertEquals(players.size(), 6);
+		
+		// test the name of a player
+		assertEquals("professor plum", players.get(0).getName());
+		assertEquals("mrs white", players.get(5).getName());
+		
+		// test the color of a player
+		assertEquals(Color.magenta, players.get(0).getColor());
+		assertEquals(Color.yellow, players.get(4).getColor());
+		
+		// test the starting location of a player
+		assertEquals(0, players.get(0).getX());
+		assertEquals(3, players.get(0).getY());
+		assertEquals(8, players.get(2).getX());
+		assertEquals(20, players.get(2).getY());
+		
+	}
+
+}
